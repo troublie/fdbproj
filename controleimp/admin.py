6deaa955f0b3ade95db81ae6a21d6c0fe6a2938c
Juliano Moraes <troublie@gmail.com>
2017-11-16 22:20:02 -0200

    Inicio

--- controleimp/admin.py
+++ controleimp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Cliente, Pedido
+from .models import Cliente, Pedido, Moeda, TermoPagto
 
 admin.site.register(Cliente)
 admin.site.register(Pedido)
+admin.site.register(Moeda)
+admin.site.register(TermoPagto)
--- controleimp/models.py
+++ controleimp/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from decimal import Decimal
+
+class TermoPagto(models.Model):
+    termo = models.IntegerField
+
+class Moeda(models.Model):
+    codigo = models.CharField(max_length=3)
+    descricao = models.CharField(max_length=30)
 
 
 class Cliente(models.Model):
@@ -18,9 +26,10 @@ class Pedido(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     numero = models.CharField(max_length=20)
     data = models.DateTimeField
-    moeda = models.CharField(max_length=3)
-    valor = models.DecimalField
-    termo = models.CharField(max_length=3)
+    moeda = models.ForeignKey(Moeda, on_delete=models.CASCADE)
+    valor = models.DecimalField(max_digits=8, decimal_places=2,
+               default=Decimal(0.00))
+    termo = models.ForeignKey(TermoPagto, on_delete=models.CASCADE)
 
     def save(self):
         self.data = timezone.now()
