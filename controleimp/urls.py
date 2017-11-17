8b7b30c60cc98bfdb70020e217fb04a5d0ac1cb3
Juliano Moraes Lopes <juliano.lopes@FujidoBrasil.local>
2017-11-17 14:57:42 -0200

    Cadastro moeda

--- controleimp/urls.py
+++ controleimp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^cadastros/moeda_list', views.moeda_list, name="moeda_list"),
     url(r'^cadastros/', views.cadastros, name="cadastros"),
     url(r'^pedidos/', views.pedidos, name="pedidos"),
 ]
--- controleimp/views.py
+++ controleimp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Moeda
 
 
 def index(request):
@@ -11,3 +13,8 @@ def cadastros(request):
 
 def pedidos(request):
     return render(request, "controleimp/pedidos.html")
+
+
+def moeda_list(request):
+    moedas = Moeda.objects.filter().order_by("codigo")
+    return render(request, "controleimp/moeda_list.html", {"moedas": moedas})
