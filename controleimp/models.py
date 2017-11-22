2d1cd4c9d592516fbb2011e55710d8c0838bf7b2
Juliano Moraes <troublie@gmail.com>
2017-11-21 22:49:34 -0200

    Sync

--- controleimp/models.py
+++ controleimp/models.py
@@ -40,7 +40,7 @@ class Pedido(models.Model):
     author = models.ForeignKey('auth.User')
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     numero = models.CharField(max_length=20)
-    data = models.DateTimeField(default=datetime.now)
+    data = models.DateTimeField(default=timezone.now)
     moeda = models.ForeignKey(Moeda, on_delete=models.CASCADE)
     valor = models.DecimalField(max_digits=8, decimal_places=2,
                                 default=Decimal(0.00))
